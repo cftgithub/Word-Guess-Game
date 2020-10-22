6138e3077a87694da572d781e5611b8451f05298
cftgithub <chiafongt@gmail.com>
2020-10-22 12:23:53 -0600

    improved UX; remove guess word from array; reordered set timeout to display image before alert message

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,18 +19,15 @@ var losses = 0;
 
 function startGame() {
     wordSelected = wordOptions[Math.floor(Math.random() * wordOptions.length)];
-    // might want to remove the selected element from the wordOptions array
-
-
-    // IF wordOptions.length === 0 array is empty, reload the array
-
+    wordIndex = wordOptions.indexOf(wordSelected);
+    wordOptions.splice(wordIndex, 1); //removes wordSelected from wordOptions array
+    checkArray(); // IF wordOptions.length === 0 array is empty, reload the array
     lettersInWord = wordSelected.split("");
     numBlanks = lettersInWord.length;
     dashWord = [];
     for (var i = 0; i < numBlanks; i++) {
         dashWord.push("_");
     }
-
 
     // Reset for new game
     blanksAndGuesses = [];
@@ -44,11 +41,15 @@ function startGame() {
     document.getElementById("lossCount").innerHTML = losses;
     document.getElementById("wrongGuesses").innerHTML = lettersGuessed;
 
-    // Testing
-    console.log(wordSelected);
-    console.log(lettersInWord);
-    console.log(numBlanks);
-    console.log(dashWord);
+}
+
+function checkArray() {
+    if (wordOptions.length === 0) {
+        wordOptions = ["pizza", "popcorn", "spaghetti", "pancakes", "soup", "waffle",
+            "burrito", "biscuit", "sushi", "hamburger", "toast", "steaks", "sausage",
+            "salmon", "churros", "quiche", "bruschetta", "cupcake", "kabob",
+            "salad", "sandwich"]
+    }
 }
 
 function resetImage() {
@@ -62,8 +63,6 @@ startGame();
 document.onkeyup = function () {
     var userGuess = event.key.toLowerCase();
     var keyCode = event.keyCode;
-    console.log(userGuess);
-    console.log(keyCode);
 
     if (keyCode >= 65 && keyCode <= 90) {
         if (lettersGuessed.includes(userGuess) === false) { //check if lettersGuessed is in userGuess array
@@ -77,28 +76,28 @@ document.onkeyup = function () {
                     }
                 }
                 if (dashWord.join("") === wordSelected) {
-                    setTimeout(function () { alert("You WIN!!!") }, 10);
+                    document.getElementById("image").setAttribute("src", "assets/images/" + wordSelected + ".jpg");
+                    setTimeout(function () { alert("You WIN!!!") }, 500);
                     wins = wins + 1;
                     document.getElementById("winCount").innerHTML = wins;
-                    document.getElementById("image").setAttribute("src", "assets/images/" + wordSelected + ".jpg");
                     //startGame ();
-                    setTimeout(function () { startGame() }, 11);
+                    setTimeout(function () { startGame() }, 1000);
                     return;
                 }
             } else {
                 guessRemain = guessRemain - 1;
                 document.getElementById("guessLeft").innerHTML = guessRemain;
                 if (guessRemain === 0) {
-                    alert("Better luck next time");
                     losses = losses + 1;
                     document.getElementById("lossCount").innerHTML = losses;
                     image = document.getElementById("image");
                     image.src = "assets/images/shouting-emoji2.png";
-                    setTimeout(function () { startGame(), resetImage() }, 2000);
+                    setTimeout(function () { alert("Better luck next time") }, 500);
+                    setTimeout(function () { startGame(), resetImage() }, 1000);
                 }
             }
         } else {
-            alert("You already pressed that letter!");        
+            alert("You already pressed that letter!");
         }
     } else {
         alert("Please enter a letter from a-z!");
